1120aab8224162b638ecd693a2ce9fe2054fb425
andrey.mikhailov <andrey.mikhailov@taskdata.ru>
2018-04-09 15:33:20 +0300

    3 page complete

--- src/test/java/second/SecondTest.java
+++ src/test/java/second/SecondTest.java
@@ -22,8 +22,10 @@ public class SecondTest extends BaseTest {
         new Select(chromeDriver.findElement(By.xpath("//*[@name='toPort']"))).selectByValue(destination);
         chromeDriver.findElement(By.cssSelector("input.btn")).click();
 
+        // TODO заменить говнолокатор
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".table")));
 
+        // TODO подумать, как уменьшить кол-во кода
         ArrayList<WebElement> pricesElem = (ArrayList<WebElement>) chromeDriver.findElements(By.xpath("//input[@name='price']"));
         ArrayList priceInt = new ArrayList();
         for (WebElement price: pricesElem) {
@@ -33,6 +35,22 @@ public class SecondTest extends BaseTest {
         elemMin.findElement(By.xpath("./..//input[@value='Choose This Flight']")).click();
 
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#inputName")));
+
+        chromeDriver.findElement(By.cssSelector("#inputName")).sendKeys("Andrey");
+        chromeDriver.findElement(By.cssSelector("#address")).sendKeys("Street, 123");
+        chromeDriver.findElement(By.cssSelector("#city")).sendKeys("S-Pb");
+        chromeDriver.findElement(By.cssSelector("#state")).sendKeys("SZFO");
+        chromeDriver.findElement(By.cssSelector("#zipCode")).sendKeys("0123456789");
+        new Select(chromeDriver.findElement(By.cssSelector("#cardType"))).selectByValue("amex");
+        chromeDriver.findElement(By.cssSelector("#creditCardNumber")).sendKeys("0123456789");
+        WebElement creditCardMonth = chromeDriver.findElement(By.cssSelector("#creditCardMonth"));
+        creditCardMonth.clear();
+        creditCardMonth.sendKeys("99");
+        WebElement creditCardYear = chromeDriver.findElement(By.cssSelector("#creditCardYear"));
+        creditCardYear.clear();
+        creditCardYear.sendKeys("0000");
+        chromeDriver.findElement(By.cssSelector("#nameOnCard")).sendKeys("AndreyM");
+        chromeDriver.findElement(By.xpath("//input[@value='Purchase Flight']")).click();
 
 
     }
